723faa6e7956dbde654b15c7282ce6dc1ea8bb8f
rostik3110 <rostikzero3110.2004@gmail.com>
2024-03-25 02:44:04 +0200

    update

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar} from "react-chartjs-2";
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const data = {
+  labels: "Balance",
+  datasets: [
+    {
+      label: "balance",
+      data: "data1",
+      backgroundColor: "lightBlue",
+      borderColor: "transparent",
+      borderRadius: "5",
+      hoverBackgroundColor: "#5932EA",
+      hoverOffset: 44,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  scales: {
+    x: { beginAtZero: true, grid: { display: false } },
+    y: { beginAtZero: true, grid: { display: false }, display: false },
+  },
+};
 
 function BarChart() {
   return (
-    <div>
-        Chance of Rain
+    <div style={{marginLeft: "30px"}}>
+        <Bar data={data} options={options}></Bar>
     </div>
   )
 }
---
+++ src/components/DayForecst.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DayForecst() {
+  return (
+    <div></div>
+  )
+}
+
+export default DayForecst
